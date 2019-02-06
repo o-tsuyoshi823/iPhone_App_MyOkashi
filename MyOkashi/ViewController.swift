3971bb7a39d8d87280bbb73eb57e95c548ccb85a
o.tsuyoshi823 <o.tsuyoshi823@gmail.com>
2019-02-06 09:52:40 +0900

    Initial Commit

---
+++ MyOkashi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOkashi
+//
+//  Created by tsuyoshi on 2019/02/06.
+//  Copyright © 2019 tsuyoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
